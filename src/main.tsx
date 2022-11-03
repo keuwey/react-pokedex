d00670755eaf74e5172d37c32010cfb7b252ade4
keuwey <keuwey@protonmail.com>
2022-11-03 14:17:52 -0300

    Consertando problema com o github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { HashRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux";
 
